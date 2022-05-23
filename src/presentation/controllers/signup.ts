37443726d57d45756ca9df6c7bf5205f99adb10d
Jhon Santana <jhonsstn@gmail.com>
2022-05-23 00:57:36 -0300

    refactor(SignUpController): add a protocol to the class

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,10 @@
 import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 import { HttpRequest } from '../protocols/http'
 
-export class SignUpController {
+export class SignUpController implements Controller {
+  // TODO: Change the "any" type for "HttpResponse" when the handle method gets a return
   handle (httpRequest: HttpRequest): any {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
