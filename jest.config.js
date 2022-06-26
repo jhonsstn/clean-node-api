92239d4159f8e82552a508e578133a6d664037d5
Jhon Santana <jhonsstn@gmail.com>
2022-06-26 18:38:55 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
   coverageDirectory: 'coverage',
   transform: {
     '.+\\.ts$': 'ts-jest'
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -43,6 +43,7 @@ export class SignUpController implements Controller {
 
       return success(account)
     } catch (error) {
+      console.error(error)
       return serverError()
     }
   }
