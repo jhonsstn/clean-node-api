324340fe2dbea45cdd385c72e68a82e3dbddb669
Jhon Santana <jhonsstn@gmail.com>
2022-07-18 17:49:56 -0300

    test(BcryptAdapter): ensure returns a encrypted password on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('encrypted_password')
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('should call bcrypt with correct values', async () => {
     const salt = 12
@@ -8,5 +14,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('should return a encrypted password if success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const encryptedPassword = await sut.encrypt('any_value')
+    expect(encryptedPassword).toBe('encrypted_password')
   })
 })
