f9881ddbbc5296860876880942b692a628c82cd4
Jhon Santana <jhonsstn@gmail.com>
2022-07-18 19:14:39 -0300

    refactor(BcryptAdapter): change test from jest mock to spyOn implementation

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,12 +1,6 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
-jest.mock('bcrypt', () => ({
-  async hash (): Promise<string> {
-    return await Promise.resolve('encrypted_password')
-  }
-}))
-
 const SALT = 12
 
 const makeSut = (): BcryptAdapter => {
@@ -23,7 +17,12 @@ describe('Bcrypt Adapter', () => {
 
   test('should return a encrypted password if success', async () => {
     const sut = makeSut()
-    const encryptedPassword = await sut.encrypt('any_value')
-    expect(encryptedPassword).toBe('encrypted_password')
+    jest
+      .spyOn(bcrypt, 'hash')
+      .mockImplementationOnce(async (): Promise<string> => {
+        return await Promise.resolve('encrypted_password')
+      })
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('encrypted_password')
   })
 })
