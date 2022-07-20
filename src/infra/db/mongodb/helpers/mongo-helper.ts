56023cadf8f21dd1b751d0fe1b6820ac140c486f
Jhon Santana <jhonsstn@gmail.com>
2022-07-20 13:51:39 -0300

    feat(AccountMongoRepository): add mongodb methods helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,11 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  connection: null as unknown as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.connection = await MongoClient.connect(uri)
+  },
+  async disconnect (): Promise<void> {
+    await this.connection.close()
+  }
+}
