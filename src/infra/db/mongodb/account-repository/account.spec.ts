58bd81701975372a34a3b7d3ed9a5ddcbf2c5767
Jhon Santana <jhonsstn@gmail.com>
2022-07-20 17:11:48 -0300

    refactor(AccountMongoRepository): clear db between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -6,6 +6,12 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  }
+  )
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
