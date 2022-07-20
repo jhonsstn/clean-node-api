23ee9c66158310467b155b89d0ef7b42f1896756
Jhon Santana <jhonsstn@gmail.com>
2022-07-20 15:37:28 -0300

    test(AccountMongoRepository): ensure returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('should return an account on success ', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toEqual({
+      id: expect.any(String),
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+  })
+})
