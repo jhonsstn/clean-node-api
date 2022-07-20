38d91593cb2e99c22b480803692c3a8b760d2c71
Jhon Santana <jhonsstn@gmail.com>
2022-07-20 20:02:44 -0300

    feat: ensure requests returns json as default content type

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 
-export const cors = (req: Request, res: Response, next: NextFunction): void => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', '*')
-  res.header('Access-Control-Allow-Methods', '*')
+export const cors = (_req: Request, res: Response, next: NextFunction): void => {
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Headers', '*')
+  res.set('Access-Control-Allow-Methods', '*')
   next()
 }
