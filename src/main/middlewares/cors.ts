992ae84efe551c3b5fe3e10f63c26b1c818f50b1
Jhon Santana <jhonsstn@gmail.com>
2022-07-20 19:42:14 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  next()
+}
