6388b38722258365cd16511371ad3cf86d2b37e8
Jhon Santana <jhonsstn@gmail.com>
2022-07-20 13:25:04 -0300

    chore: add jest mongodb setup

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
   coverageDirectory: 'coverage',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
