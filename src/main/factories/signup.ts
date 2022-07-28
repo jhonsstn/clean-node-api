4224ff67fb8a79b256c53785299c7ca37c0099b0
Jhon Santana <jhonsstn@gmail.com>
2022-07-27 22:49:30 -0300

    feat: add SignUpController factory method

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidator = new EmailValidatorAdapter()
+  const encrypter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const addAccount = new DbAddAccount(encrypter, accountMongoRepository)
+  return new SignUpController(emailValidator, addAccount)
+}
