4f39b4583a45dfce4b7ae954dedd5e8aba209370
Jhon Santana <jhonsstn@gmail.com>
2022-08-13 21:45:27 -0300

    feat: add LogMongoRepository to SignUp factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,17 +1,11 @@
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-error-repository/log-error'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
-
-class LogErrorRepositoryStub implements LogErrorRepository {
-  async logError (_stack: string): Promise<void> {
-    return await Promise.resolve()
-  }
-}
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -20,5 +14,6 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(encrypter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidator, addAccount)
-  return new LogControllerDecorator(signUpController, new LogErrorRepositoryStub())
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
